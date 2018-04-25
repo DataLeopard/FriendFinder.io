75c5f1785b68f912ea9c604d48d660b4f51c354a
Alan Burke <alanburke311@gmail.com>
2018-04-25 18:09:23 -0500

    Add files via upload

---
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -0,0 +1,14 @@
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    // res.send("Welcome to the Star Wars Page!")
+    res.sendFile(path.join(__dirname, "newview.html"));
+  });
+
+  /* Your `htmlRoutes.js` file should include two routes:
+
+   * A GET Route to `/survey` which should display the survey page.
+   * A default, catch-all route that leads to `home.html` which displays the home page. 
+ */
